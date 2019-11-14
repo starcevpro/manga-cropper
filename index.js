822f813b8f65d889f0895f1729aa2298d771fd94
norice31@gmail.com <admin@server.168.0.11>
2019-11-14 12:16:01 +0300

    Suka

--- index.js
+++ index.js
@@ -40,8 +40,13 @@
             var manga = manga
             const spinner = ora("Crop "+manga).start();
             var source = this.manga+'/'+manga
-            var x = [208, 208*2, 208*3, 208*4]
-            var y = [296, 296*2, 296*3, 296*4]
+
+            var width = 208,
+                height = 296,
+                white = 12
+
+            var x = [width, width*2, width*3, width*4]
+            var y = [height, height*2, height*3, height*4]
 
             its(source, y, x, {
                 clipperOptions: {
@@ -59,21 +64,25 @@
                     { src: Buffer.from(img[5], 'base64')},
                     { src: Buffer.from(img[10], 'base64')},
                     { src: Buffer.from(img[15], 'base64')},
+                    { src: Buffer.from(img[4], 'base64')},
 
-                    { src: Buffer.from(img[1], 'base64'), offsetX: -208*4, offsetY: 296},
-                    { src: Buffer.from(img[6], 'base64'), offsetY: 296},
-                    { src: Buffer.from(img[11], 'base64'), offsetY: 296},
-                    { src: Buffer.from(img[16], 'base64'), offsetY: 296},
+                    { src: Buffer.from(img[1], 'base64'), offsetX: -white-width*4, offsetY: height},
+                    { src: Buffer.from(img[6], 'base64'), offsetY: height},
+                    { src: Buffer.from(img[11], 'base64'), offsetY: height},
+                    { src: Buffer.from(img[16], 'base64'), offsetY: height},
+                    { src: Buffer.from(img[9], 'base64'), offsetY: height},
 
-                    { src: Buffer.from(img[2], 'base64'), offsetX: -208*4, offsetY: 296*2},
-                    { src: Buffer.from(img[7], 'base64'), offsetY: 296*2},
-                    { src: Buffer.from(img[12], 'base64'), offsetY: 296*2},
-                    { src: Buffer.from(img[17], 'base64'), offsetY: 296*2},
+                    { src: Buffer.from(img[2], 'base64'), offsetX: -white-width*4, offsetY: height*2},
+                    { src: Buffer.from(img[7], 'base64'), offsetY: height*2},
+                    { src: Buffer.from(img[12], 'base64'), offsetY: height*2},
+                    { src: Buffer.from(img[17], 'base64'), offsetY: height*2},
+                    { src: Buffer.from(img[14], 'base64'), offsetY: height*2},
 
-                    { src: Buffer.from(img[3], 'base64'), offsetX: -208*4, offsetY: 296*3},
-                    { src: Buffer.from(img[8], 'base64'), offsetY: 296*3},
-                    { src: Buffer.from(img[13], 'base64'), offsetY: 296*3},
-                    { src: Buffer.from(img[18], 'base64'), offsetY: 296*3}
+                    { src: Buffer.from(img[3], 'base64'), offsetX: -white-width*4, offsetY: height*3},
+                    { src: Buffer.from(img[8], 'base64'), offsetY: height*3},
+                    { src: Buffer.from(img[13], 'base64'), offsetY: height*3},
+                    { src: Buffer.from(img[18], 'base64'), offsetY: height*3},
+                    { src: Buffer.from(img[19], 'base64'), offsetY: height*3},
                 ]).then((img) => {
                     img.write(this.croped+'/'+manga, () => {
                         spinner.succeed(manga + " saved!")
